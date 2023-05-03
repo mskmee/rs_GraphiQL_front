5b2623b01ebf048cd696c829ce46d94e2e420301
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-03 20:32:31 +0300

    feat: add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
 import { WelcomePage } from '@/components/WelcomePage/WelcomePage';
+import { NotFoundPage } from '@/components/NotFoundPage/NotFoundPage';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '@/components/Layout/Layout';
+import { EditorPage } from '@/components/EditorPage/EditorPage';
 
 function App() {
-  return <WelcomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="/editor" element={<EditorPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,33 @@
 import { Button } from '@/components/BasicComponents/Button';
 import styles from './Header.module.css';
 import logo from '@/assets/icons/graphiQL-logo.png';
+import { NavLink } from 'react-router-dom';
 
-export const Header = () => {
+interface HeaderProps {
+  openSignIn: () => void;
+  openSignUp: () => void;
+}
+
+export const Header = ({ openSignIn, openSignUp }: HeaderProps) => {
   return (
     <header className={styles.wrapper}>
-      <a href="" className={styles.logo}>
+      <NavLink className={styles.logo} to="/">
         <img className={styles.logoImage} src={logo} alt="GraphiQL logo" />
-      </a>
+      </NavLink>
       <div className={styles.buttonsWrapper}>
-        <Button text="Sign In" />
-        <Button buttonClass={styles.signUpButton} text="Sign Up" />
+        <Button
+          text="Sign In"
+          onClick={() => {
+            openSignIn();
+          }}
+        />
+        <Button
+          buttonClass={styles.signUpButton}
+          text="Sign Up"
+          onClick={() => {
+            openSignUp();
+          }}
+        />
       </div>
     </header>
   );
