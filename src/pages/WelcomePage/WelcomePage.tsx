fe715549d684226d70f32e3d057feaa559b2db89
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-04 15:28:27 +0300

    feat: make WelcomePage components separate

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -2,10 +2,9 @@ import styles from './WelcomePage.module.css';
 import { Header } from '@/components/Header/Header';
 import bgImage from '@/assets/images/graphiQL-bg.jpg';
 import arrowImage from '@/assets/images/arrow.png';
-import rsLogo from '@/assets/images/rs-logo.png';
-import { Authors } from './Authors';
 import { useCallback, useState } from 'react';
 import { Login } from './Login';
+import { Info } from './Info';
 
 export const WelcomePage = () => {
   const [isLoginOpen, setIsLoginOpen] = useState('');
@@ -28,20 +27,7 @@ export const WelcomePage = () => {
       <img className={styles.arrowImage} src={arrowImage} alt="Arrow" draggable="false" />
       <Header openSignIn={openSignIn} openSignUp={openSignUp} />
       <Login status={isLoginOpen} closeLoginPage={closeLoginPage} />
-      <div className={styles.info}>
-        <div className={styles.about}>
-          GraphiQL is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-        <Authors />
-        <div className={styles.sponsors}>
-          <p>Sponsored by</p>
-          <a href="https://rs.school/" target="_blank" rel="noreferrer">
-            <img className={styles.rsImage} src={rsLogo} alt="The Rolling Scopes School" />
-          </a>
-        </div>
-      </div>
+      <Info />
     </div>
   );
 };
