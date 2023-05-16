c2bfc04b51795584a769695e6c14bbeeeb25aa4a
Maksimenko Maksim <mskmee2@gmail.com>
2023-05-16 15:16:54 +0300

    refactor: use effect dependenci

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,23 @@ import { EditorPage } from '@/pages/EditorPage/EditorPage';
 import { NotFoundPage } from '@/pages/NotFoundPage/NotFoundPage';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from '@/components/Layout/Layout';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeIsUserLogged, changeUserName } from './store/stateSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(changeIsUserLogged(true));
+        dispatch(changeUserName(user.displayName ?? 'Unknown'));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
