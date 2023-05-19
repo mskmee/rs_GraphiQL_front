1bc2b67dfb245a13da2ff369ada650149d3259dc
Maksimenko Maksim <mskmee2@gmail.com>
2023-05-19 20:20:18 +0300

    feat: default values to json pase

--- src/api/apiController.ts
+++ src/api/apiController.ts
@@ -4,10 +4,10 @@ import { IApiController } from '@/types/interfaces/IApiController';
 
 export const apiController: IApiController = {
   getSchema: () => api(getIntrospectionQuery()),
-  getGraphQLResponse: function (query, headersString, variablesString) {
-    // const headers = JSON.parse(`{${headersString}}`) as object;
-    // const variables = JSON.parse(`{${variablesString}}`) as object;
-    console.log(query);
-    return api(query);
+  getGraphQLResponse: function (query, headersString = '', variablesString = '') {
+    //todo check entry
+    const headers = JSON.parse(`{${headersString}}`) as object;
+    const variables = JSON.parse(`{${variablesString}}`) as object;
+    return api(query, headers, variables);
   },
 };
