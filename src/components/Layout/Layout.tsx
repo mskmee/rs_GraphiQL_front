15389d6ac4c629898c44465379b427ad3e54666c
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-20 17:51:38 +0300

    feat: add adaptive styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/ReactToastify.min.css';
 import { Header } from '@/components/Header/Header';
+import { Footer } from '@/components/Footer/Footer';
 
 export const Layout = () => {
   return (
@@ -18,6 +19,7 @@ export const Layout = () => {
           draggable
         />
       </main>
+      <Footer />
     </>
   );
 };
