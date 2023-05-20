d11f51c9b0badb9d405aeb74629e0e551090ba4c
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-20 17:51:14 +0300

    feat: add header color change on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from './Header.module.css';
+import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { useAppSelector } from '@/hooks/useRedux';
 import { SingComponent, LoggedComponent } from './singModules';
 import logo from '@/assets/icons/graphiQL-logo.png';
@@ -8,8 +9,23 @@ import logo from '@/assets/icons/graphiQL-logo.png';
 export const Header = memo(function Header() {
   const isUserLogged = useAppSelector((state) => state.userState.isUserLogged);
 
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsScrolled(scrollPosition > 0);
+    };
+
+    window.onscroll = handleScroll;
+
+    return () => {
+      window.onscroll = null;
+    };
+  }, []);
+
   return (
-    <header className={styles.wrapper}>
+    <header className={classNames(styles.wrapper, { [styles.headerScroll]: isScrolled })}>
       <NavLink className={styles.logo} to="/">
         <img className={styles.logoImage} src={logo} alt="GraphiQL logo" />
       </NavLink>
