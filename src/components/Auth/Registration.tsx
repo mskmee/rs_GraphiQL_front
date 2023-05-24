96a9fe9896a859f50a500575895b1afa0b2b02a9
Maksimenko Maksim <mskmee2@gmail.com>
2023-05-24 11:50:05 +0300

    feat: registration and reset password form

--- src/components/Auth/Registration.tsx
+++ src/components/Auth/Registration.tsx
@@ -1,5 +1,87 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import { changeIsUserLogged, changeLoginStatus, changeUserName } from '@/store/userSlice';
+import { useAppDispatch } from '@/hooks/useRedux';
+import styles from './Styles.module.css';
+import { Input, Link, Button } from '@/components/BasicComponents';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { auth } from '@/db';
+import { Loader } from '@/components/Loader';
+import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useTranslation } from 'react-i18next';
+import { SignUpData, signUpSchema } from '@/pages/Login/loginComponents/FormValidation';
+import registrationImg from '@/assets/images/registration-img.jpg';
 
 export const Registration = () => {
-  return <div>Registration</div>;
+  const { t } = useTranslation();
+  const signUpButtonValue = t('login.signUp');
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<SignUpData>({
+    resolver: yupResolver(signUpSchema),
+  });
+
+  const dispatch = useAppDispatch();
+  const nameRef = useRef<string | null>(null);
+  const name = nameRef.current;
+  const [createUser, user, isLoading, error] = useCreateUserWithEmailAndPassword(auth);
+  const [updateProfile, isUpdateLoading, updateError] = useUpdateProfile(auth);
+
+  useEffect(() => {
+    const updateUserInBD = async (name: string) => {
+      const isUpdate = await updateProfile({ displayName: name });
+      if (isUpdate) {
+        dispatch(changeIsUserLogged(true));
+        dispatch(changeUserName(name));
+        dispatch(changeLoginStatus(''));
+      }
+    };
+    if (error) {
+      toast(error.message, { type: 'error' });
+    }
+    if (updateError) {
+      toast(updateError.message, { type: 'error' });
+    }
+    if (user && name) {
+      updateUserInBD(name).catch((err) => toast(err.message, { type: 'error' }));
+    }
+  }, [error, updateError, updateProfile, dispatch, name, user]);
+
+  const onSubmit = async (data: SignUpData) => {
+    nameRef.current = data.name;
+    await createUser(data.email, data.password);
+    reset();
+  };
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.img} src={registrationImg} alt="registration image" />
+
+      {(isLoading || isUpdateLoading) && <Loader />}
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <Input label="name" register={register} errors={errors} />
+        <Input label="email" register={register} errors={errors} />
+        <Input label="password" register={register} errors={errors} />
+        <Input label="repeatPassword" register={register} errors={errors} />
+        <Button type="submit" className={styles.submitButton}>
+          <input type="submit" value={signUpButtonValue} className={styles.submitInput} />
+        </Button>
+        <div className={styles.select}>
+          {t('login.haveAccount')}
+          <Link
+            linkStyle="bold"
+            text={t('login.signIn')}
+            onClick={() => {
+              dispatch(changeLoginStatus('sign-in'));
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
--- src/components/Auth/ResetPassword.tsx
+++ src/components/Auth/ResetPassword.tsx
@@ -1,5 +1,66 @@
-import React from 'react';
+import { auth } from '@/db';
+import styles from './Styles.module.css';
+import { useAppDispatch } from '@/hooks/useRedux';
+import { ResetData, resetSchema } from '@/pages/Login/loginComponents/FormValidation';
+import { changeLoginStatus } from '@/store/userSlice';
+import { useEffect } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { Loader } from '../Loader';
+import { Button, Input, Link } from '../BasicComponents';
+import { useTranslation } from 'react-i18next';
+import { yupResolver } from '@hookform/resolvers/yup';
+import emailImg from '@/assets/images/forgot-password-img.jpg';
 
 export const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+  const { t } = useTranslation();
+  const sendButtonValue = t('login.send');
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<ResetData>({
+    resolver: yupResolver(resetSchema),
+  });
+
+  const dispatch = useAppDispatch();
+  const [resetPassword, isLoading, error] = useSendPasswordResetEmail(auth);
+
+  useEffect(() => {
+    if (error) {
+      toast(error.message, { type: 'error' });
+    }
+  }, [error]);
+
+  const onSubmit = async (data: ResetData) => {
+    const isResetSuccess = await resetPassword(data.email);
+    if (isResetSuccess) {
+      toast('Check instructions in you email.', { type: 'success' });
+      dispatch(changeLoginStatus(''));
+    }
+    reset();
+  };
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.img} src={emailImg} alt="login image" />
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        {isLoading && <Loader />}
+        <Input required label="email" register={register} errors={errors} />
+        <Button type="submit" className={styles.submitButton}>
+          <input type="submit" value={sendButtonValue} className={styles.submitInput} />
+        </Button>
+        <Link
+          linkStyle="bold"
+          text={t('login.return')}
+          onClick={() => {
+            dispatch(changeLoginStatus('sign-in'));
+          }}
+        />
+      </form>
+    </div>
+  );
 };
