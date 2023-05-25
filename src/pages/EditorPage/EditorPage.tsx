5e6a19a9c20727a50f45c7b028f48334d5160249
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-25 15:42:34 +0300

    feat: add docs

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -5,17 +5,25 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '@/components/BasicComponents/ErrorFallback';
 import { toast } from 'react-toastify';
 import { Loader } from '@/components/Loader';
+import { Docs } from './Docs/Docs';
+import { useQuery } from '@tanstack/react-query';
+import { apiController } from '@/api/apiController';
 
 const onError = (error: Error) => {
   toast(error.message, { type: 'error' });
 };
 
-export const EditorPage = () => (
-  <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
-    <React.Suspense fallback={<Loader />}>
-      <div className={styles.wrapper}>
-        <Editor />
-      </div>
-    </React.Suspense>
-  </ErrorBoundary>
-);
+export const EditorPage = () => {
+  const schemaResponse = useQuery(['getSchema'], apiController.getSchema);
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+      <React.Suspense fallback={<Loader />}>
+        <div className={styles.wrapper}>
+          <Docs schemaResponse={schemaResponse} />
+          <Editor schemaResponse={schemaResponse} />
+        </div>
+      </React.Suspense>
+    </ErrorBoundary>
+  );
+};
