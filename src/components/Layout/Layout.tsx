85037cca28e752291f474b82451c1cd1206ed842
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-25 15:40:35 +0300

    fix: adjust styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import styles from './Layout.module.css';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/ReactToastify.min.css';
@@ -8,7 +9,7 @@ export const Layout = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Outlet />
         <ToastContainer
           position="bottom-left"
