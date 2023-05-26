4f83f5a27645361c4e7e810b67833a8407b6151b
mkoroleva5 <mkoroleva5@gmail.com>
2023-05-26 13:28:52 +0300

    feat: divide docs into separate components

--- src/pages/EditorPage/Docs/Docs.tsx
+++ src/pages/EditorPage/Docs/Docs.tsx
@@ -4,11 +4,11 @@ import classNames from 'classnames';
 import docsIcon from '@/assets/icons/archive.svg';
 import { UseQueryResult } from '@tanstack/react-query';
 import { GraphQLField, GraphQLSchema } from 'graphql';
-import { RootTypesIcon, TypesIcon } from './DocsIcons';
 import { Types } from './Types';
 import { BackButton } from './BackButton';
 import { GraphQLNonNestedType } from '@/types/GraphQLNonNestedType';
-import { useTranslation } from 'react-i18next';
+import { RootTypes } from './RootTypes';
+import { AllRootTypes } from './AllRootTypes';
 
 interface DocsProps {
   schemaResponse: UseQueryResult<GraphQLSchema, unknown>;
@@ -22,13 +22,11 @@ export type HistoryType =
   | { element: GraphQLField<unknown, unknown>; type: 'field' };
 
 export const Docs = ({ schemaResponse }: DocsProps) => {
-  const { t } = useTranslation();
   const [isDocsOpen, setIsDocsOpen] = useState(false);
   const [history, setHistory] = useState<ReadonlyArray<HistoryType>>([]);
 
   const schema = schemaResponse.data;
   const queryType = schema?.getQueryType() || null;
-  const typeMap = schema?.getTypeMap() as object;
 
   const onHistoryPush = (el: HistoryType) => {
     setHistory([...history, el]);
@@ -56,44 +54,8 @@ export const Docs = ({ schemaResponse }: DocsProps) => {
           <div className={styles.docsInfo}>
             {history.length === 0 && (
               <div className={styles.typesWrapper}>
-                <div className={styles.subtitle}>
-                  <RootTypesIcon />
-                  {t('docs.rootTypes')}
-                </div>
-                <div className={styles.query}>
-                  query:
-                  <button
-                    className={classNames(styles.button, styles.fieldButton)}
-                    type="button"
-                    onClick={() => {
-                      setHistory([...history, { element: queryType, type: 'type' }]);
-                    }}
-                  >
-                    {queryType.name}
-                  </button>
-                </div>
-                <div className={styles.subtitle}>
-                  <TypesIcon />
-                  {t('docs.allTypes')}
-                </div>
-                <div>
-                  {Object.values(typeMap)
-                    .filter((el) => el.name[0] !== '_')
-                    .map((el) => {
-                      return (
-                        <button
-                          key={el.name}
-                          className={classNames(styles.button, styles.fieldButton)}
-                          type="button"
-                          onClick={() => {
-                            setHistory([...history, { element: el, type: 'type' }]);
-                          }}
-                        >
-                          {el.name}
-                        </button>
-                      );
-                    })}
-                </div>
+                <RootTypes queryType={queryType} onHistoryPush={onHistoryPush} />
+                <AllRootTypes schema={schema} onHistoryPush={onHistoryPush} />
               </div>
             )}
             {history.length > 0 && (
