5bb488c400886b25a40566482b2087299102cf4a
Maksimenko Maksim <mskmee2@gmail.com>
2023-05-27 13:51:55 +0300

    refactor: useAuth Listener

--- src/hooks/useAuthListener.ts
+++ src/hooks/useAuthListener.ts
@@ -1,31 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from './useRedux';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { changeIsUserLogged, changeUserName } from '@/store/userSlice';
 
 export const useAuthListener = () => {
-  const [wasUserLogged, setWasUserLogged] = useState(false);
   const isLogged = useAppSelector((state) => state.userState.isUserLogged);
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useAppDispatch();
-  const isUserWillNavigate = wasUserLogged && location.pathname === '/editor';
+  const isUserWillNavigate = isLogged && location.pathname === '/editor';
 
   useEffect(() => {
     const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        setWasUserLogged(true);
+        dispatch(changeIsUserLogged(true));
         return;
       }
       if (isUserWillNavigate) {
-        setWasUserLogged(false);
         navigate('/');
         dispatch(changeIsUserLogged(false));
         dispatch(changeUserName(''));
       }
     });
+    return unsubscribe;
   }, [dispatch, isUserWillNavigate, navigate]);
 
   return isLogged;
